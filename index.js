53f2c1af4858031ca2eb8ad530d64d6127774a0b
Mateusz Jasiak <mateusz.jasiak.dev@gmail.com>
2022-04-28 00:12:06 +0200

    Fix the delete method

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.delete("/invitations/:id", async function(req, res) {
     try {
         const id = req.params.id;
         await Invitation.findByIdAndDelete(id);
-        res.status(200).json({"success": true, "message": "Player invitations have been removed."});
+        res.send("The player invite has been removed.");
     } catch (error) {
         res.status(400).json({"success": false, "message": "Error removing a player invite. Error: " + error});
         console.log("Error removing a player invite. Error: " + error);
